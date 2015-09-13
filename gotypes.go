26207212c96fca8fe2ed5fdfe04624794e2b3dd9
Sergey Fedosenko <sergey.fedosenko@lazada.com>
2015-09-14 01:00:00 +0300

    Filler fix

--- gotypes.go
+++ gotypes.go
@@ -11,36 +11,36 @@ const (
 )
 
 type Converter struct {
-	input      interface{}
-	output     interface{}
-	zeros      map[string]bool
-	errors     []string
-	calculated bool
-	validated  bool
+	input           interface{}
+	output          interface{}
+	allowZeroFields map[string]bool
+	invalidFields   []string
+	calculated      bool
+	validated       bool
 }
 
 func NewConverter(input interface{}, output interface{}) *Converter {
 	return &Converter{
-		input:  input,
-		output: output,
-		zeros:  map[string]bool{},
-		errors: []string{},
+		input:           input,
+		output:          output,
+		allowZeroFields: map[string]bool{},
+		invalidFields:   []string{},
 	}
 }
 
 func (c *Converter) Valid() bool {
 	if !c.validated {
-		c.validateAny(reflect.Indirect(reflect.ValueOf(c.output)), "")
+		c.validate(reflect.ValueOf(c.output), "", "")
 		c.validated = true
 	}
 
-	return len(c.errors) == 0
+	return len(c.invalidFields) == 0
 }
 
 func (c *Converter) GetInvalidFields() []string {
 	c.calculation()
 
-	return c.errors
+	return c.invalidFields
 }
 
 func (c *Converter) GetInput() interface{} {
@@ -76,192 +76,246 @@ func (c *Converter) getName(field reflect.StructField) string {
 	return name
 }
 
-func (c *Converter) isZero(value reflect.Value) bool {
-	switch value.Kind() {
-	case reflect.Chan, reflect.Func, reflect.Map, reflect.Ptr, reflect.Interface, reflect.Slice:
-		return value.IsNil()
-
-	case reflect.Struct:
-		z := true
-		for i := 0; i < value.NumField(); i++ {
-			z = z && c.isZero(value.Field(i))
-		}
-		return z
-	}
-
-	if value.CanInterface() {
-		return reflect.Zero(value.Type()).Interface() == value.Interface()
-	}
-
-	return false
-}
-
-func (c *Converter) setZero(val reflect.Value, path string) {
-	if path != "" && c.isZero(val) {
-		c.zeros[path] = true
-	}
-}
-
 func (c *Converter) calculation() {
 	if c.calculated {
 		return
 	}
 
-	c.fill(reflect.Indirect(reflect.ValueOf(c.output)), c.input, "")
+	value := reflect.Indirect(reflect.ValueOf(c.output))
+
+	c.findAllowZeroFields(value, "")
+	c.fillOutput(value, c.input, "")
+	c.calculated = true
 }
 
-func (c *Converter) fill(out reflect.Value, in interface{}, path string) {
-	if out.Kind() != reflect.Ptr && out.Kind() != reflect.Interface {
-		c.setZero(out, path)
-	}
+func (c *Converter) fillOutput(output reflect.Value, input interface{}, path string) {
+	switch output.Kind() {
 
-	switch out.Kind() {
 	case reflect.Ptr:
-		if out.IsValid() {
-			if out.IsNil() {
-				out.Set(reflect.New(out.Type().Elem()))
+		if output.IsValid() {
+			if output.IsNil() {
+				output.Set(reflect.New(output.Type().Elem()))
 			}
 
-			c.fill(out.Elem(), in, path)
+			c.fillOutput(output.Elem(), input, path)
 		}
 
 	case reflect.Interface:
-		c.fill(out.Elem(), in, path)
+		c.fillOutput(output.Elem(), input, path)
 
-	case reflect.Struct:
-		if in, ok := in.(map[string]interface{}); ok {
-			for i := 0; i < out.NumField(); i++ {
-				name := c.getName(out.Type().Field(i))
-				childPath := c.getPath(path, name)
+	case reflect.Map:
+		inputValue := reflect.ValueOf(input)
+		values := map[string]interface{}{}
 
-				if value, ok := in[name]; ok {
-					c.fill(out.Field(i), value, childPath)
-				}
+		switch inputValue.Kind() {
+		case reflect.Map:
+			for _, n := range inputValue.MapKeys() {
+				values[n.String()] = inputValue.MapIndex(n).Interface()
+			}
+
+		case reflect.Struct:
+			for i := 0; i < inputValue.NumField(); i++ {
+				field := inputValue.Type().Field(i)
+				values[field.Name] = inputValue.FieldByName(field.Name).Interface()
 			}
 		}
 
-	case reflect.Map:
-		if in, ok := in.(map[string]interface{}); ok {
-			keyType := out.Type().Key()
-			valueType := out.Type().Elem()
+		if len(values) > 0 {
+			var value reflect.Value
 
-			out.Set(reflect.MakeMap(out.Type()))
-			for mapKey, mapValue := range in {
+			keyType := output.Type().Key()
+			valueType := output.Type().Elem()
+
+			output.Set(reflect.MakeMap(output.Type()))
+			for i := range values {
 				key := reflect.New(keyType).Elem()
-				c.fill(key, mapKey, path)
+				c.fillOutput(key, i, path)
 
-				value := reflect.New(valueType).Elem()
-				childPath := c.getPath(path, fmt.Sprintf("[%q]", mapKey))
-				c.fill(value, mapValue, childPath)
+				if valueType.Kind() != reflect.Interface {
+					value = reflect.New(valueType).Elem()
+					childPath := c.getPath(path, fmt.Sprintf("[%q]", i))
+					c.fillOutput(value, values[i], childPath)
+				} else {
+					value = reflect.ValueOf(values[i])
+				}
 
-				out.SetMapIndex(key, value)
+				output.SetMapIndex(key, value)
 			}
 		}
 
 	case reflect.Slice:
-		if in, ok := in.([]interface{}); ok {
-			out.Set(reflect.MakeSlice(out.Type(), len(in), cap(in)))
-			for i := range in {
-				c.fill(out.Index(i), in[i], c.getPath(path, fmt.Sprintf("[%d]", i)))
+		inputValue := reflect.ValueOf(input)
+
+		if inputValue.Kind() == reflect.Slice {
+			output.Set(reflect.MakeSlice(output.Type(), inputValue.Len(), inputValue.Cap()))
+
+			for i := 0; i < output.Len(); i++ {
+				c.fillOutput(output.Index(i), inputValue.Index(i).Interface(), c.getPath(path, fmt.Sprintf("[%d]", i)))
+			}
+		}
+
+	case reflect.Struct:
+		inputValue := reflect.ValueOf(input)
+		values := map[string]interface{}{}
+
+		switch inputValue.Kind() {
+		case reflect.Map:
+			for _, n := range inputValue.MapKeys() {
+				values[n.String()] = inputValue.MapIndex(n).Interface()
+			}
+
+		case reflect.Struct:
+			for i := 0; i < inputValue.NumField(); i++ {
+				field := inputValue.Type().Field(i)
+				values[field.Name] = inputValue.FieldByName(field.Name).Interface()
+			}
+		}
+
+		if len(values) > 0 {
+			for i := 0; i < output.NumField(); i++ {
+				name := c.getName(output.Type().Field(i))
+				childPath := c.getPath(path, name)
+
+				if value, ok := values[name]; ok {
+					c.fillOutput(output.Field(i), value, childPath)
+				}
 			}
 		}
 
 	case reflect.Bool:
-		out.SetBool(ToBool(in))
+		output.SetBool(ToBool(input))
 
 	case reflect.String:
-		out.SetString(ToString(in))
+		output.SetString(ToString(input))
 
 	case reflect.Uint:
-		out.Set(reflect.ValueOf(ToUint(in)))
+		output.Set(reflect.ValueOf(ToUint(input)))
 
 	case reflect.Uint8:
-		out.Set(reflect.ValueOf(ToUint8(in)))
+		output.Set(reflect.ValueOf(ToUint8(input)))
 
 	case reflect.Uint16:
-		out.Set(reflect.ValueOf(ToUint16(in)))
+		output.Set(reflect.ValueOf(ToUint16(input)))
 
 	case reflect.Uint32:
-		out.Set(reflect.ValueOf(ToUint32(in)))
+		output.Set(reflect.ValueOf(ToUint32(input)))
 
 	case reflect.Uint64:
-		out.SetUint(ToUint64(in))
+		output.SetUint(ToUint64(input))
 
 	case reflect.Int:
-		out.Set(reflect.ValueOf(ToInt(in)))
+		output.Set(reflect.ValueOf(ToInt(input)))
 
 	case reflect.Int8:
-		out.Set(reflect.ValueOf(ToInt8(in)))
+		output.Set(reflect.ValueOf(ToInt8(input)))
 
 	case reflect.Int16:
-		out.Set(reflect.ValueOf(ToInt16(in)))
+		output.Set(reflect.ValueOf(ToInt16(input)))
 
 	case reflect.Int32:
-		out.Set(reflect.ValueOf(ToInt32(in)))
+		output.Set(reflect.ValueOf(ToInt32(input)))
 
 	case reflect.Int64:
-		out.SetInt(ToInt64(in))
+		output.SetInt(ToInt64(input))
 
 	case reflect.Float32:
-		out.Set(reflect.ValueOf(ToFloat32(in)))
+		output.Set(reflect.ValueOf(ToFloat32(input)))
 
 	case reflect.Float64:
-		out.SetFloat(ToFloat64(in))
+		output.SetFloat(ToFloat64(input))
 
-	default:
 	}
 }
 
-func (c *Converter) validateAny(out reflect.Value, path string) {
-	out = reflect.Indirect(out)
-	if !out.IsValid() {
-		return
-	}
-
-	switch out.Kind() {
-	case reflect.Struct:
-		c.validateStruct(out, path)
-
-	case reflect.Slice:
-		for i := 0; i < out.Len(); i++ {
-			c.validateAny(out.Index(i), c.getPath(path, fmt.Sprintf("[%d]", i)))
-		}
+func (c *Converter) findAllowZeroFields(output reflect.Value, path string) {
+	switch output.Kind() {
 
 	case reflect.Map:
-		for _, n := range out.MapKeys() {
-			c.validateAny(out.MapIndex(n), c.getPath(path, fmt.Sprintf("[%q]", n.String())))
+		for _, n := range output.MapKeys() {
+			c.findAllowZeroFields(output.MapIndex(n), c.getPath(path, fmt.Sprintf("[%q]", n.String())))
 		}
+
+	case reflect.Ptr:
+		if path != "" {
+			c.allowZeroFields[path] = true
+		}
+
+	case reflect.Slice:
+		value := reflect.New(output.Type().Elem()).Elem()
+		c.findAllowZeroFields(value, c.getPath(path, "[]"))
+
+	case reflect.Struct:
+		for i := 0; i < output.NumField(); i++ {
+			field := output.Type().Field(i)
+			fieldPath := c.getPath(path, c.getName(field))
+
+			tag := field.Tag.Get("json")
+			if tag != "" {
+				parts := strings.Split(tag, ",")
+				if len(parts) > 1 && parts[1] == "omitempty" {
+					c.allowZeroFields[fieldPath] = true
+					continue
+				}
+			}
+
+			c.findAllowZeroFields(output.FieldByName(field.Name), fieldPath)
+		}
+
 	}
 }
 
-func (c *Converter) validateStruct(value reflect.Value, path string) {
-	for i := 0; i < value.Type().NumField(); i++ {
-		f := value.Type().Field(i)
-		val := value.FieldByName(f.Name)
+func (c *Converter) validate(output reflect.Value, path string, fieldPath string) {
+	output = reflect.Indirect(output)
+	valid := true
 
-		name := c.getName(f)
-		childPath := c.getPath(path, name)
-		valid := true
+	switch output.Kind() {
+	case reflect.Struct:
+		for i := 0; i < output.NumField(); i++ {
+			field := output.Type().Field(i)
+			val := output.FieldByName(field.Name)
 
-		if val.Kind() != reflect.Ptr {
-			switch val.Kind() {
-			case reflect.Chan, reflect.Func, reflect.Map, reflect.Ptr, reflect.Interface, reflect.Slice:
-				if val.IsNil() {
-					valid = false
-				}
-			default:
-				if !val.IsValid() {
-					valid = false
-				} else if c.isZero(val) {
-					_, valid = c.zeros[childPath]
-				}
+			subPath := c.getPath(path, c.getName(field))
+			subFieldPath := c.getPath(fieldPath, c.getName(field))
+
+			c.validate(val, subPath, subFieldPath)
+		}
+
+	case reflect.Slice:
+		valid = !output.IsNil()
+
+		if valid {
+			for i := 0; i < output.Len(); i++ {
+				subPath := c.getPath(path, "[]")
+				subFieldPath := c.getPath(fieldPath, fmt.Sprintf("[%d]", i))
+
+				c.validate(output.Index(i), subPath, subFieldPath)
 			}
 		}
 
+	case reflect.Map:
+		valid = !output.IsNil()
+
 		if valid {
-			c.validateAny(val, childPath)
-		} else {
-			c.errors = append(c.errors, childPath)
+			for _, n := range output.MapKeys() {
+				subPath := c.getPath(path, fmt.Sprintf("[%q]", n.String()))
+				subFieldPath := c.getPath(fieldPath, fmt.Sprintf("[%q]", n.String()))
+
+				c.validate(output.MapIndex(n), subPath, subFieldPath)
+			}
+		}
+
+	case reflect.Chan, reflect.Func, reflect.Interface:
+		valid = !output.IsNil()
+
+	default:
+		if !output.IsValid() || output.CanInterface() && reflect.Zero(output.Type()).Interface() == output.Interface() {
+			valid = false
+		}
+	}
+
+	if !valid && path != "" {
+		if _, ok := c.allowZeroFields[path]; !ok {
+			c.invalidFields = append(c.invalidFields, fieldPath)
 		}
 	}
 }
